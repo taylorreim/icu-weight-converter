d7090b6670df0aba706041ee244011e570d59221
taylorreim <taylorreim2@gmail.com>
2026-03-20 01:19:33 -0400

    Add drip rate calculator and Enter key support

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function convert(){
   let rounded = kg.toFixed(1);
   document.getElementById("result").innerText = lbs + "lbs=" + rounded + " kg";
 }
+function calculateDrip() {
+  let dose = document.getElementById("doseInput").value;
+  let weight = document.getElementById("weightKgInput").value;
+  let concentration = document.getElementById("concentrationInput").value;
+
+  let mlPerHour = (dose * weight * 60) / concentration;
+  let rounded = mlPerHour.toFixed(1);
+
+  document.getElementById("dripResult").innerText = 
+    "Drip rate: " + rounded + " ml/hr";
+}
+document.getElementById("weightInput").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    convert();
+  }
+});
+
+document.getElementById("concentrationInput").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    calculateDrip();
+  }
+});
